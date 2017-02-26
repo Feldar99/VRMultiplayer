c600fef841ed53e8e9ff1b29ea91a855a0801996
Paul Schwarzwalder <pschwarz99@gmail.com>
2017-02-26 13:39:01 -0800

    Attempted to connect sounds, then rolled back

--- Assets/DoorKnob.cs
+++ Assets/DoorKnob.cs
@@ -4,19 +4,49 @@ using UnityEngine;
 
 public class DoorKnob : MonoBehaviour
 {
-    public bool Locked = true;
+    [SerializeField]
+    private bool locked = true;
+    private bool success = false;
+    private float successTimer = 0;
+
+    public AudioClip unlockSound;
+    public AudioClip successSound;
+
+    private AudioSource audio;
+
+    public bool Locked
+    {
+        get { return locked; }
+        set
+        {
+            if (!value && locked)
+            {
+                PlaySound(unlockSound);
+            }
+            locked = value;
+        }
+    }
 
     // Use this for initialization
-    void Start() {}
+    void Start()
+    {
+        audio = GetComponent<AudioSource>();
+    }
 
     // Update is called once per frame
     void Update() {}
 
     void OnTriggerEnter(Collider col)
     {
-        if (!Locked && col.CompareTag("Little"))
+        if (!locked && col.CompareTag("Little"))
         {
             GameObject.FindGameObjectWithTag("GameController").GetComponent<GameController>().NextLevel();
         }
+    }
+
+    private void PlaySound(AudioClip sound)
+    {
+        audio.clip = sound;
+        audio.Play();
     }
 }

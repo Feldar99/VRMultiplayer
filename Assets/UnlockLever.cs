10e85e7923644c347fafe864629486c913689a93
Charles Paul Harris-White <Charlespaul.hw@gmail.com>
2017-02-25 22:00:55 -0800

    Scene 3 with Levers

---
+++ Assets/UnlockLever.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using NewtonVR;
+
+public class UnlockLever : MonoBehaviour {
+	[SerializeField]
+	private DoorKnob doorknob ;
+
+
+	// Use this for initialization
+	void Start () {
+
+	}
+
+	// Update is called once per frame
+	void Update () {
+		if (GetComponents<NVRLever> ().LeverEngaged) {
+			doorknob.Locked = false;
+		}
+	}
+}
---
+++ Assets/UnlockedLeverGroup.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UnlockedLeverGroup : MonoBehaviour {
+
+	[SerializeField]
+	private DoorKnob doorknob;
+	public UnlockLever[] levers;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		foreach (UnlockLever lever in levers) {
+			if (!lever.UnlockedPosition) {
+				return;
+			}
+		}
+
+	doorknob.Locked = false;
+
+	}
+}

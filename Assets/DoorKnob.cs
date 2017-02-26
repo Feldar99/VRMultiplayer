293cae43eaacec3f95d77db34fc3dbcfba7edba8
Paul Schwarzwalder <pschwarz99@gmail.com>
2017-02-25 17:31:12 -0800

    Added door

---
+++ Assets/DoorKnob.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DoorKnob : MonoBehaviour
+{
+    public bool Locked = true;
+
+    // Use this for initialization
+    void Start() {}
+
+    // Update is called once per frame
+    void Update() {}
+
+    void OnTriggerEnter(Collider col)
+    {
+        if (!Locked && col.CompareTag("Little"))
+        {
+            GameObject.FindGameObjectWithTag("GameController").GetComponent<GameController>().NextLevel();
+        }
+    }
+}
---
+++ Assets/GameController.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameController : MonoBehaviour
+{
+    public string[] scenes;
+    private int sceneIndex;
+
+    private static GameController instance;
+
+    // Use this for initialization
+    void Start()
+    {
+        if (instance == null)
+        {
+            instance = this;
+            GameObject.DontDestroyOnLoad(this);
+        }
+        else
+        {
+            GameObject.Destroy(this);
+        }
+    }
+
+    public void NextLevel()
+    {
+        sceneIndex = (sceneIndex + 1) % scenes.Length;
+        SceneManager.LoadScene(scenes[sceneIndex]);
+    }
+}

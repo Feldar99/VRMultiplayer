8adbb2d1ffbe8bb7ab9225fd9f8c35aa6494c0f5
Charles Paul Harris-White <Charlespaul.hw@gmail.com>
2017-02-25 20:50:22 -0800

    Added Text to wall, added button, connected button to door lock

---
+++ Assets/UnlockButton.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using NewtonVR;
+
+public class UnlockButton : MonoBehaviour {
+	[SerializeField]
+	private DoorKnob doorknob ;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (GetComponent<NVRButton> ().ButtonIsPushed) {
+			doorknob.Locked = false;
+		}
+		}
+}
